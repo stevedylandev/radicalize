df7db7268eb1dcde5208b4a22d51935b55ff6bfa
Steve <stevedsimkins@gmail.com>
2024-08-22 00:55:12 -0400

    finished remote action

--- RemoteClone.go
+++ RemoteClone.go
@@ -7,18 +7,20 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"os/exec"
 	"strings"
 
 	"github.com/AlecAivazis/survey/v2"
+	"github.com/fatih/color"
 )
 
 func RemoteClone() error {
-	fmt.Print("Enter the name of GitHub user or org")
+	fmt.Print("Enter the name of GitHub user or org: ")
 
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
 	if err != nil {
-		fmt.Println("An error occured while reading the input. Please try again", err)
+		fmt.Println("An error occurred while reading the input. Please try again", err)
 		return err
 	}
 	input = strings.TrimSuffix(input, "\n")
@@ -27,9 +29,10 @@ func RemoteClone() error {
 
 	response, err := http.Get(apiURL)
 	if err != nil {
-		fmt.Println("Github API request failed with error", err)
+		fmt.Println("GitHub API request failed with error", err)
 		return err
 	}
+	defer response.Body.Close()
 
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
@@ -44,14 +47,8 @@ func RemoteClone() error {
 		return err
 	}
 
-	newRepos := selectNewRepos(repos)
-
-	for _, repo := range newRepos {
-		fmt.Printf("Repository: %s\n", repo.FullName)
-		fmt.Printf("Description: %s\n", repo.Description)
-		fmt.Printf("URL: %s\n", repo.HTMLURL)
-		fmt.Printf("---\n")
-	}
+	selectedRepos := selectNewRepos(repos)
+	confirmAndCloneRepos(selectedRepos)
 
 	return nil
 }
@@ -64,7 +61,7 @@ func selectNewRepos(repos []Repository) []Repository {
 
 	var selected []string
 	prompt := &survey.MultiSelect{
-		Message: "Select repositories to initialize:",
+		Message: "Select repositories to clone and initialize:",
 		Options: options,
 	}
 	survey.AskOne(prompt, &selected)
@@ -80,4 +77,51 @@ func selectNewRepos(repos []Repository) []Repository {
 	}
 
 	return selectedRepos
+}
+
+func confirmAndCloneRepos(repos []Repository) {
+	confirm := false
+	prompt := &survey.Confirm{
+		Message: fmt.Sprintf("Clone and initialize %d repositories?", len(repos)),
+	}
+	survey.AskOne(prompt, &confirm)
+
+	if !confirm {
+		fmt.Println("Radicalization cancelled.")
+		return
+	}
+
+	fmt.Printf("Cloning and initializing %d repositories...\n", len(repos))
+
+	for i, repo := range repos {
+		fmt.Printf("Processing %s (%d/%d)...\n", repo.Name, i+1, len(repos))
+
+		// Clone the repository
+		err := cloneRepo(repo.CloneURL, repo.Name)
+		if err != nil {
+			color.Red("Error cloning %s: %v\n", repo.Name, err)
+			continue
+		}
+
+		// Initialize with rad
+		err = runRadInitRemote(repo.Name, repo.Name)
+		if err != nil {
+			color.Red("Error initializing %s: %v\n", repo.Name, err)
+		} else {
+			color.Green("Cloned and initialized %s\n", repo.Name)
+		}
+	}
+
+	fmt.Println("Radicalization Complete")
+}
+
+func cloneRepo(url, name string) error {
+	cmd := exec.Command("git", "clone", url, name)
+	return cmd.Run()
+}
+
+func runRadInitRemote(path, name string) error {
+	cmd := exec.Command("rad", "init", "--name", name, "--description", "", "--public", "--no-confirm")
+	cmd.Dir = path
+	return cmd.Run()
 }
